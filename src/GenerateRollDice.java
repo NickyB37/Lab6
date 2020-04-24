24d8a6de7e18deeef1749576dcb62962680825b2
kevin <boonic1@icloud.com>
2020-04-24 08:07:05 -0400

    commit Lab6

---
+++ src/GenerateRollDice.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class GenerateRollDice {
+
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+
+		
+
+		char runApp = 'y';
+		while (runApp == 'y') {
+			System.out.println("Enter number of sides for dice 1:");
+
+			int side1 = scnr.nextInt();
+			System.out.println("Enter number of sides for dice 2:");
+			int side2 = scnr.nextInt();
+
+			int point = generateRandomDieRoll(side1) + generateRandomDieRoll(side2);
+			System.out.println("Your point is:" + point);
+			if (point == 12 || point == 3 || point == 2) {
+				System.out.println("Craps");
+			} else if (point == 7 || point == 11) {
+				System.out.println("winner");
+			}
+
+		}
+		System.out.println("Roll Again (y/n)");
+		runApp = scnr.next().charAt(0);
+	}
+
+	public static int generateRandomDieRoll(int dieRoll) {
+		int die1;
+		die1 = (int) (Math.random() * dieRoll + 1);
+		int die2;
+		die2 = (int) (Math.random() * dieRoll + 1);
+
+		return die1;
+
+	}
+
+}
